1cfaef1977a2acd0cc730fa076db8f745abfc23a
Allan Almeida <82304884+drstone-genius04@users.noreply.github.com>
2026-09-01 16:19:14 -0400

    Reorder authors in copyright statement

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'empanada-napari-v1.2'
-copyright = '2026 Allan Almeida,  Abhishek Bhardwaj, Kedar Narayan ,Madeline Barry, Mariam Demir and Ryan Conrad'
+copyright = '2026 Allan Almeida, Abhishek Bhardwaj, Madeline Barry, Mariam Demir, Ryan Conrad and Kedar Narayan'
 author = 'Allan Almeida'
 release = '1.2.4'
 
